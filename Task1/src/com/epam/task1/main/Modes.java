79d583fe6dac0ef75ccf6a34c5e68984011995c5
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-22 21:50:13 +0300

    Update Modes.java

--- Task1/src/com/epam/task1/main/Modes.java
+++ Task1/src/com/epam/task1/main/Modes.java
@@ -1,5 +1,6 @@
 package com.epam.task1.main;
 
+//лучше переделай этот класс в enum
 public final class Modes {
     public static final String CREATE = "1";
     public static final String COUNT = "2";
