c86be5770663f4004a4c67a3b6e92f05f6295add
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-22 21:51:51 +0300

    Update Chef.java

--- Task1/src/com/epam/task1/main/Chef.java
+++ Task1/src/com/epam/task1/main/Chef.java
@@ -2,6 +2,8 @@ package com.epam.task1.main;
 
 import com.epam.task1.manager.SaladManager;
 
+//этот класс лучше переименовать в Controller, Runner или Starter
+//пакет лучше переименовать также (в Controller, Runner или Starter) только с маленикой буквы
 public class Chef {
     public static void main(String[] args) {
         Controller controller = new Controller(new SaladManager());
